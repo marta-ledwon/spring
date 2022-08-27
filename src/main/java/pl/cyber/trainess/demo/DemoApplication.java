6bdaff220c668716d87db2a71390bcd6c69563ec
marta-ledwon <marta-ledwon@users.noreply.github.com>
2022-08-27 09:09:03 +0200

    inicjalizacja SpringBootApplication

---
+++ src/main/java/pl/cyber/trainess/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package pl.cyber.trainess.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // adnotacja ktorej zadaniem jest uruchomienie aplikacji Spring
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args );
+    }
+}
